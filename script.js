2416ac9ef614b58b3a334ecb10de8a11b4444703
Kosma Pajor <110028823+kosmaelpe@users.noreply.github.com>
2022-09-19 13:48:25 +0200

    All in - for calculator

---
+++ script.js
@@ -0,0 +1,115 @@
+//przypisanie
+
+const currentNumber = document.querySelector(".currentNumber");
+
+const previousNumber = document.querySelector(".previousNumber p");
+
+const mathSign = document.querySelector(".mathSign");
+
+const clearButton = document.querySelector(".clear");
+
+const operatorsButtons = document.querySelectorAll(".operator");
+
+const numbersButtons = document.querySelectorAll(".number");
+
+const equalsButton = document.querySelector(".equals");
+
+const historyBtn = document.querySelector(".historyBtn");
+
+const calculatorHistory = document.querySelector(".history");
+
+// domyślnie pusty wynik
+let result = "";
+
+function displayNumbers() {
+  if (this.textContent === "." && currentNumber.innerHTML.includes(".")) return;
+  if (this.textContent === "." && currentNumber.innerHTML === "")
+    return (currentNumber.innerHTML = "0.");
+
+  currentNumber.innerHTML += this.textContent;
+}
+
+function operate() {
+  if (currentNumber.innerHTML === "" && this.textContent === "-") {
+    currentNumber.innerHTML = "-";
+    return;
+  } else if (currentNumber.innerHTML === "") {
+    return;
+  }
+
+  if (mathSign.innerHTML !== "") {
+    showResult();
+  }
+  previousNumber.innerHTML = currentNumber.innerHTML;
+  mathSign.innerHTML = this.textContent;
+  currentNumber.innerHTML = "";
+}
+
+function showResult() {
+  if (previousNumber.innerHTML === "" || currentNumber.innerHTML === "") return;
+  let a = Number(currentNumber.innerHTML);
+  let b = Number(previousNumber.innerHTML);
+  let operator = mathSign.innerHTML;
+
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+
+    case "-":
+      result = b - a;
+      break;
+
+    case "x":
+      result = a * b;
+      break;
+
+    case ":":
+      result = b / a;
+      break;
+
+    case "2^":
+      result = b ** a;
+      break;
+  }
+
+  addToHistory();
+  historyBtn.classList.add("active");
+  currentNumber.innerHTML = result;
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
+}
+
+function addToHistory() {
+  const newHistoryItem = document.createElement("li");
+  newHistoryItem.innerHTML = `${currentNumber.innerHTML} ${mathSign.innerHTML} ${previousNumber.innerHTML} = ${result}`;
+  newHistoryItem.classList.add("history-item");
+  calculatorHistory.appendChild(newHistoryItem);
+}
+
+function clearScreen() {
+  result = " ";
+  currentNumber.innerHTML = "";
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
+}
+
+function clearHistory() {
+  calculatorHistory.textContent = "";
+  historyBtn.classList.remove("active");
+}
+
+//
+//
+//nasłuch
+operatorsButtons.forEach((button) => button.addEventListener("click", operate));
+
+equalsButton.addEventListener("click", showResult);
+
+clearButton.addEventListener("click", clearScreen);
+
+numbersButtons.forEach((button) => {
+  button.addEventListener("click", displayNumbers);
+});
+
+historyBtn.addEventListener("click", clearHistory);
